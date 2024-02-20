1be77d43235f9099e21a515299d4bb66bf9790d6
CH S S Swetha <81875681+SwethaChagantii@users.noreply.github.com>
2024-02-20 13:05:51 +0530

    Time: 42 ms (23.36%) | Memory: 16.5 MB (99.09%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,15 @@
+from collections import Counter
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        braces = {"{":"}","[":"]","(":")"}
+        for char in s:
+            if char in braces:
+                stack.append(char)
+            elif stack and char == braces[stack[-1]]:
+                stack.pop()
+            else:
+                return False
+        return not stack
+
+        
