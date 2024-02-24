64ad10717003b3f1050bc610693634d484cc324c
CH S S Swetha <81875681+SwethaChagantii@users.noreply.github.com>
2024-02-25 00:47:10 +0530

    Time: 75 ms (67.76%) | Memory: 17.9 MB (47.04%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -6,13 +6,10 @@ class Solution:
         for i in range(n):
             if nums[i] == target:
                 li.append(i)
-        if len(li) > 2:
+        if len(li) >= 1:
             li2 += [min(li),max(li)]
             li = li2
-        if len(li) == 1:
-            li.append(li[0])
-        if target not in nums:
-                li.append(-1)
-                li.append(-1)
+        if len(li) == 0:
+                li += [-1,-1]
         return li
         
