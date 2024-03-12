3e38549ef32fc6d08c498c9d71b879fecbab0f58
CH S S Swetha <81875681+SwethaChagantii@users.noreply.github.com>
2024-03-12 21:46:00 +0530

    Time: 32 ms (80.44%) | Memory: 16.7 MB (31.40%) - LeetSync

--- 263-ugly-number/ugly-number.py
+++ 263-ugly-number/ugly-number.py
@@ -3,8 +3,9 @@ class Solution:
         if n <= 0:
             return False
         
-        for prime in [2, 3, 5]:
+        for prime in [2,3,5]:
             while n % prime == 0:
-                n //= prime
-        
+                n /= prime
         return n == 1
+
+
