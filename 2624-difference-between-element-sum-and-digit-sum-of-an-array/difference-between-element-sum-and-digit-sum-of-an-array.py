548f779358414f30f4305380c524636ef775f451
CH S S Swetha <81875681+SwethaChagantii@users.noreply.github.com>
2024-03-14 22:59:40 +0530

    Time: 98 ms (93.96%) | Memory: 16.9 MB (38.03%) - LeetSync

--- 2624-difference-between-element-sum-and-digit-sum-of-an-array/difference-between-element-sum-and-digit-sum-of-an-array.py
+++ 2624-difference-between-element-sum-and-digit-sum-of-an-array/difference-between-element-sum-and-digit-sum-of-an-array.py
@@ -10,10 +10,6 @@ class Solution:
         tot1 = 0
         for num in nums:
             tot1 += self.sum_digit(num)
-        
-        
         tot2 = sum(nums)
-        print(tot1,tot2)
-
         return abs(tot2-tot1)
         
