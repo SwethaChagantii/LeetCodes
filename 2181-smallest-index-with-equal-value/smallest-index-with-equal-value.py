b43845bca9bafb69f0e9f4382bf7abf5ab24dcc2
CH S S Swetha <81875681+SwethaChagantii@users.noreply.github.com>
2024-03-20 23:33:54 +0530

    Time: 84 ms (7.64%) | Memory: 16.5 MB (73.27%) - LeetSync

--- 2181-smallest-index-with-equal-value/smallest-index-with-equal-value.py
+++ 2181-smallest-index-with-equal-value/smallest-index-with-equal-value.py
@@ -3,7 +3,6 @@ class Solution:
         dic = {key: None for key in nums}
         res = -1
         for i in range(len(nums)):
-            print(i,i%10,nums[i])
             if i % 10 == nums[i]:
                 res = i
                 break
